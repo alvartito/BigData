2a2a38653f53fd966577378cb07b905df0552414
Alvaro <Alvaro@192.168.1.37>
2014-12-23 12:08:53 +0100

    .

--- alvaro-sanchez-blasco/src/main/java/alvaro/sanchez/blasco/util/analisis/Contador.java
+++ alvaro-sanchez-blasco/src/main/java/alvaro/sanchez/blasco/util/analisis/Contador.java
@@ -1,4 +1,4 @@
-package analisis;
+package alvaro.sanchez.blasco.util.analisis;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
---
+++ alvaro-sanchez-blasco/src/main/java/alvaro/sanchez/blasco/util/writables/MRWritable.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package alvaro.sanchez.blasco.util.writables;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.WritableComparable;
+
+/**
+ * @author Alvaro
+ *
+ */
+public class MRWritable implements WritableComparable<MRWritable> {
+
+	@Override
+	public void readFields(DataInput arg0) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void write(DataOutput arg0) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int compareTo(MRWritable o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

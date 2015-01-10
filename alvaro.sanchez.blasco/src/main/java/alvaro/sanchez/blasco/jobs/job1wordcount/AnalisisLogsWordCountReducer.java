6ac89a88897527d75584328ad8ff0dc8b678c833
cloudera <cloudera@quickstart.cloudera>
2015-01-10 21:20:25 +0100

    .

--- alvaro.sanchez.blasco/src/main/java/alvaro/sanchez/blasco/jobs/job1wordcount/AnalisisLogsWordCountReducer.java
+++ alvaro.sanchez.blasco/src/main/java/alvaro/sanchez/blasco/jobs/job1wordcount/AnalisisLogsWordCountReducer.java
@@ -3,9 +3,7 @@ package alvaro.sanchez.blasco.jobs.job1wordcount;
 import java.io.IOException;
 
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 
 import alvaro.sanchez.blasco.writables.FechaHoraProcesoWritableComparable;
 
@@ -17,8 +15,6 @@ public class AnalisisLogsWordCountReducer
 		extends
 		Reducer<FechaHoraProcesoWritableComparable, IntWritable, FechaHoraProcesoWritableComparable, IntWritable> {
 
-	private MultipleOutputs<FechaHoraProcesoWritableComparable, IntWritable> multipleOut;
-
 	@Override
 	public void reduce(FechaHoraProcesoWritableComparable key,
 			Iterable<IntWritable> values, Context context) throws IOException,
@@ -27,33 +23,6 @@ public class AnalisisLogsWordCountReducer
 		for (IntWritable value : values) {
 			wordCount += value.get();
 		}
-		// context.write(key, new IntWritable(wordCount));
-		multipleOut.write(key, new IntWritable(wordCount), generateFileName(key));
-	}
-
-	@Override
-	protected void cleanup(Context context) throws IOException,
-			InterruptedException {
-		multipleOut.close();
-	}
-
-	@Override
-	protected void setup(Context context) throws IOException,
-			InterruptedException {
-		multipleOut = new MultipleOutputs<FechaHoraProcesoWritableComparable, IntWritable>(
-				context);
-	}
-
-	private String generateFileName(FechaHoraProcesoWritableComparable k) {
-		// expect Text k in format "Surname|Forename"
-		String dir = k.getFecha().toString();
-		String fich = k.getHora().toString();
-		
-		String tmp = dir+"/"+fich;
-
-		// example for k = Smith|John
-		// output written to /user/hadoop/path/to/output/Smith/John-r-00000
-		// (etc)
-		return tmp;
+		context.write(key, new IntWritable(wordCount));
 	}
 }
--- alvaro.sanchez.blasco/src/main/java/alvaro/sanchez/blasco/jobs/job2secondarysort/AnalisisLogsSecondarySortReducer.java
+++ alvaro.sanchez.blasco/src/main/java/alvaro/sanchez/blasco/jobs/job2secondarysort/AnalisisLogsSecondarySortReducer.java
@@ -4,23 +4,51 @@ import java.io.IOException;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 
 import alvaro.sanchez.blasco.writables.FechaHoraNumWritableComparable;
 import alvaro.sanchez.blasco.writables.ProcesoNumWritable;
 
 public class AnalisisLogsSecondarySortReducer extends
-		Reducer<FechaHoraNumWritableComparable, ProcesoNumWritable, Text, Text> {
+		Reducer<FechaHoraNumWritableComparable, ProcesoNumWritable, FechaHoraNumWritableComparable, Text> {
 	Text outputValue = new Text();
 
+	private MultipleOutputs<FechaHoraNumWritableComparable, Text> multipleOut;
+
 	@Override
-	protected void reduce(FechaHoraNumWritableComparable key, Iterable<ProcesoNumWritable> values, Context context)
+	protected void reduce(FechaHoraNumWritableComparable key,
+			Iterable<ProcesoNumWritable> values, Context context)
 			throws IOException, InterruptedException {
 		StringBuilder sb = new StringBuilder("");
-		for (ProcesoNumWritable sn: values){
+		for (ProcesoNumWritable sn : values) {
 			sb = sb.append(sn.toString()).append(",");
 		}
 		outputValue.set(sb.toString());
-		context.write(new Text(key.toString()), outputValue);
+//		context.write(key, outputValue);
+		multipleOut.write(key, outputValue, generateFileName(key));
 	}
 
+	@Override
+	protected void cleanup(Context context) throws IOException,
+			InterruptedException {
+		multipleOut.close();
+	}
+
+	@Override
+	protected void setup(Context context) throws IOException,
+			InterruptedException {
+		multipleOut = new MultipleOutputs<FechaHoraNumWritableComparable, Text>(context);
+	}
+
+	private String generateFileName(FechaHoraNumWritableComparable k) {
+		// expect Text k in format "Surname|Forename"
+		String dir = k.getFechaHora().toString();
+		String[] splits = dir.split("/");
+		
+		// example for k = Smith|John
+		// output written to /user/hadoop/path/to/output/Smith/John-r-00000
+		// (etc)
+		return splits[0];
+	}
+	
 }

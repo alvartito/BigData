250bfbed8913e33ae6ff4d90977332d76592212f
cloudera <cloudera@quickstart.cloudera>
2015-02-22 13:07:56 +0100

    Pequeñas modificaciones

--- PruebasScala/src/entregables/Ejercicio01.scala
+++ PruebasScala/src/entregables/Ejercicio01.scala
@@ -3,10 +3,19 @@ package pruebas
 /**
  * @author Álvaro Sánchez Blasco
  */
-class Ejercicio01Entregable {
-  def suma(x: Int) = x + 2
-  // a)
+class Ejercicio01Entregable extends App {
 
+  /**
+   * Define en Scala la función recursiva pura (sin tail recursion) aplica3D,
+   * que reciba una lista de coordenadas 3D en forma de tupla, una función unaria y una coordenada (“x”, “y” ó “z”). 
+   * Deberá aplicar la función a los elementos correspondientes a esa coordenada y devolver las nuevas coordenadas. 
+   * Ejemplo:
+   * val lista=List((1,2,3),(4,5,6),(7,8,9),(10,11,12))
+   * def suma(x:Int)=x+2
+   * aplica3D(lista,suma_,“y”)→ ((1,4,3),(4,7,6),
+   * (7,10,9),(10,13,12))
+   *
+   */
   def aplica3D(lista: List[(Int, Int, Int)], f: (Int) => Int, c: String): List[(Int, Int, Int)] =
     {
       if (lista.isEmpty) Nil else
@@ -15,12 +24,10 @@ class Ejercicio01Entregable {
           case "y" => (lista.head._1, f(lista.head._2), lista.head._3) :: aplica3D(lista.tail, f, c)
           case "z" => (lista.head._1, lista.head._2, f(lista.head._3)) :: aplica3D(lista.tail, f, c)
         }
-
     }
 
-  //// Pruebas
-  println("---Ejercicio 1a")
-  //
+  def suma(x: Int) = x + 2
+
   val listaE1a = List((1, 2, 3), (4, 5, 6), (7, 8, 9), (10, 11, 12))
 
   println(aplica3D(listaE1a, suma _, "y")) // => ((1,4,3), (4,7,6), (7,10,9), (10,13,12))
--- PruebasScala/src/entregables/Ejercicio02.scala
+++ PruebasScala/src/entregables/Ejercicio02.scala
@@ -1,7 +1,7 @@
 package entregables
 
 /**
- * @author cloudera
+ * @author Álvaro Sánchez
  */
 class Ejercicio02 {
   def numTests(lista: List[(Int) => Boolean], n: Int): Int =
@@ -11,32 +11,35 @@ class Ejercicio02 {
       else numTests(lista.tail, n)
     }
 
-  // Pruebas
-  println("---Ejercicio 3")
-
   def mayorQue8(n: Int) = n > 8
   def par(n: Int) = n % 2 == 0
   def impar(n: Int) = n % 2 != 0
   val listaTests = List(mayorQue8 _, par _, impar _)
   println(numTests(listaTests, 12)) // => 2
   println(numTests(listaTests, 3)) // => 1
+
   
-  def makeTest(p: String, n: Int): (Int) => Boolean =
+  
+  def generaTests(lista: List[String]): List[(Int) => Boolean] =
+    {
+      if (lista.length < 2) Nil
+      else generateTest(lista.head, lista.tail.head.toInt) :: generaTests(lista.tail.tail)
+    }
+  
+  def generateTest(p: String, n: Int): (Int) => Boolean =
     {
       p match {
         case "<" => (x: Int) => x < n
         case ">" => (x: Int) => x > n
         case "=" => (x: Int) => x == n
+        case "!" => (x: Int) => x != n
       }
     }
 
-  def generaTests(lista: List[String]): List[(Int) => Boolean] =
-    {
-      if (lista.length < 2) Nil
-      else makeTest(lista.head, lista.tail.head.toInt) :: generaTests(lista.tail.tail)
-    }
+  
 
   // Pruebas
-  val listTest = generaTests(List(">", "3", "<", "5", ">", "8", "=", "10"))
+  val listTest = generaTests(List(">", "3", "<", "5", ">", "8", "=", "10", "!", "2"))
   println(numTests(listTest, 10)) // => 3
+
 }

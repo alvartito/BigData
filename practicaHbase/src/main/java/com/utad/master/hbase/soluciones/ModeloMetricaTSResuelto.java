9b35742c4d32dc6d5bc24c25920d6671bbed49c3
Alvaro <alvartito@gmail.com>
2015-02-28 12:55:29 -0800

    .

--- practicaHbase/src/main/java/com/utad/master/hbase/soluciones/ModeloMetricaTSResuelto.java
+++ practicaHbase/src/main/java/com/utad/master/hbase/soluciones/ModeloMetricaTSResuelto.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Result;
@@ -22,6 +24,7 @@ import org.apache.hadoop.hbase.util.Bytes;
 
 /**
  * El objetivo de esta practica es usar un modelo de datos de tipo SERIE TEMPORAL EMPRESA y paginar en filas y columnas.
+ * Se paginará unicamente sobre 'price'
  * 
  * <p>Para ello se calculara la media de las cotizaciones maximas del mes 2001-01 para cada empresa
  * <p>------------------------------------------
@@ -47,6 +50,8 @@ public class ModeloMetricaTSResuelto {
 	public static final String columnFamily1 = "price";
 	
 	public static final String START_ROW = "high  /9223371055926775807";
+	
+	//Siempre es el mismo, el que se defina en los requisitos.
 	public static final String STOP_ROW = "high  /9223371058518775807";
 
 	private Configuration conf = null;
@@ -88,15 +93,15 @@ public class ModeloMetricaTSResuelto {
 		Filter filter1 = new FamilyFilter(CompareFilter.CompareOp.EQUAL, new BinaryComparator(
 				Bytes.toBytes(columnFamily1)));
 		filters.add(filter1);
-		
+
 		// filtro para que incluya la stop rowkey
 		Filter filter2 = new InclusiveStopFilter(Bytes.toBytes(STOP_ROW));
 		filters.add(filter2);
-		
+
 		// filtro de paginado por RowKey
 		Filter filter3 = new PageFilter(pageSize);
 		filters.add(filter3);
-		
+
 		// filtro de paginado por columnas
 		Filter filter4 = new ColumnPaginationFilter(pageSize, coff);
 		filters.add(filter4);
@@ -107,24 +112,31 @@ public class ModeloMetricaTSResuelto {
 		// instancia el scan y asigna los filtros
 		Scan scan = new Scan();
 		scan.setFilter(filterList);
-		
+
 		// sufijo para paginar en base a la RowKey final de la pagina anterior
 		byte[] sufijo = { 0 };
-		
+
 		// asigna la startRowKey
 		scan.setStartRow(Bytes.add(startRowKey, sufijo));
-		
+
 		// asigna la stopRowKey
 		scan.setStopRow(Bytes.toBytes(STOP_ROW));
-		
+
 		// asigna el numero maximo de versiones a 1
 		scan.setMaxVersions(1);
 
 		// instancia el iterador
 		ResultScanner scanner = tabla.getScanner(scan);
 		for (Result result : scanner) {
+			// Resultado del scanner (rango de RK)
 			System.out.println(result);
-
+			// Contenido de cada RK
+			List<Cell> celdas = result.listCells();
+			for (Cell cell : celdas) {
+				System.out.println("Key: " + cell.toString());
+				System.out.println("Value: "
+						+ Bytes.toString(CellUtil.cloneValue(cell)));
+			}
 			RowKey = result.getRow();
 		}
 		scanner.close();
@@ -148,14 +160,19 @@ public class ModeloMetricaTSResuelto {
 		
 		// tamaño de pagina
 		int pageSize = 5;
-		
+
+		/*
+		 * ESCANEAMOS PRIMERO POR FILAS, Y LUEGO POR COLUMNAS, 
+		 * EN PEQUEÑOS BLOQUES DE TAMAÑÓ 5X5
+		 * DE IZQUIERDA A DERECHA, DE ARRIBA HACIA ABAJO
+		 */
 		// RowKeystart contiene la clave inicial para cada pagina
 		// itera para paginar por filas
 		while (RowKeyStart != null) {
 			byte[] RowKeyStart1 = RowKeyStart;
 			byte[] RowKeyStart2 = RowKeyStart;
 			int coff = 0;
-			
+
 			// RowKeystart2 será null cuando haya terminado de paginar por columnas
 			// itera para paginar por columnas
 			while (RowKeyStart2 != null) {

787028228c6f07bc29d613120013c82dd18fb114
Álvaro Sánchez <alvartito@gmail.com>
2015-03-13 08:58:33 -0700

    Conexión al keyspace en paquete util

--- cassandra/src/com/utad/cassandra/basic/FirstContact.java
+++ cassandra/src/com/utad/cassandra/basic/FirstContact.java
@@ -17,64 +17,46 @@ public class FirstContact {
 
 	public static void main(String args[]) throws ConnectionException {
 
+		//Datos necesarios para realizar la conexión
 		String clusterName = "utad";
-		String keyspaceName = "utad";
+		String keyspaceName = "utad2";
 		String columnFamilyName = "users";
 
-		AstyanaxContext<Keyspace> context = new AstyanaxContext.Builder()
-				.forCluster(clusterName)
-				.forKeyspace(keyspaceName)
-				.withAstyanaxConfiguration(
-						new AstyanaxConfigurationImpl()
-								.setDiscoveryType(NodeDiscoveryType.RING_DESCRIBE))
-				.withConnectionPoolConfiguration(
-						new ConnectionPoolConfigurationImpl("MyConnectionPool")
-								.setPort(9160).setMaxConnsPerHost(1)
-								.setSeeds("127.0.0.1:9160"))
-				.withConnectionPoolMonitor(new CountingConnectionPoolMonitor())
-				.buildKeyspace(ThriftFamilyFactory.getInstance());
+		//Crear la conexión con la BD
+		AstyanaxContext<Keyspace> context = new AstyanaxContext.Builder().forCluster(clusterName).forKeyspace(keyspaceName)
+				.withAstyanaxConfiguration(new AstyanaxConfigurationImpl().setDiscoveryType(NodeDiscoveryType.RING_DESCRIBE))
+				.withConnectionPoolConfiguration(new ConnectionPoolConfigurationImpl("MyConnectionPool").setPort(9160).setMaxConnsPerHost(1).setSeeds("127.0.0.1:9160"))
+				.withConnectionPoolMonitor(new CountingConnectionPoolMonitor()).buildKeyspace(ThriftFamilyFactory.getInstance());
 
 		context.start();
+		//Crear la conexión con la BD
 
+		//Keyspace que vamos a utilizar.
 		Keyspace ksUsers = context.getClient();
 
 		try {
-			ksUsers.createKeyspace(ImmutableMap
-					.<String, Object> builder()
-					.put("strategy_options",
-							ImmutableMap.<String, Object> builder()
-									.put("replication_factor", "1").build())
+			//Creamos el keyspace, con factor de replicación 1 en Cassandra
+			ksUsers.createKeyspace(ImmutableMap.<String, Object> builder().put("strategy_options", ImmutableMap.<String, Object> builder().put("replication_factor", "1").build())
 					.put("strategy_class", "SimpleStrategy").build());
-
-			
 		} catch (Exception e) {
-			System.out.println("ya existe el keyspace " + keyspaceName);
+			System.out.println("Ya existe el keyspace: " + keyspaceName);
 		}
-		
-		
-		
-		ColumnFamily<String, String> cfUsers = new ColumnFamily<String, String>(
-				columnFamilyName, StringSerializer.get(), StringSerializer.get());
 
-		
-		
+		//Clumnfamily 
+		ColumnFamily<String, String> cfUsers = new ColumnFamily<String, String>(columnFamilyName, StringSerializer.get(), StringSerializer.get());
+
 		try {
-			ksUsers.createColumnFamily(
-					cfUsers,
-					ImmutableMap.<String, Object> builder()
-							.put("key_validation_class", "BytesType")
-							.put("comparator_type", "BytesType").build());
+			//Creamos el Clumnfamily en Cassandra 
+			ksUsers.createColumnFamily(cfUsers, ImmutableMap.<String, Object> builder().put("key_validation_class", "BytesType").put("comparator_type", "BytesType").build());
 		} catch (Exception e) {
-			System.out.println("ya existe el column family " + columnFamilyName);
+			System.out.println("Ya existe el column family: " + columnFamilyName);
 		}
 
-		// escribir un valor
-		ksUsers.prepareColumnMutation(cfUsers, "usersById", "1")
-				.putValue("user1@void.com", null).execute();
+		// Escribir un valor en Cassandra
+		ksUsers.prepareColumnMutation(cfUsers, "usersById", "1").putValue("user1@void.com", null).execute();
 
 		// leer un valor
-		Column<String> result = ksUsers.prepareQuery(cfUsers)
-				.getKey("usersById").getColumn("1").execute().getResult();
+		Column<String> result = ksUsers.prepareQuery(cfUsers).getKey("usersById").getColumn("1").execute().getResult();
 		String value = result.getStringValue();
 
 		System.out.println("email for user 1 is: " + value);
--- cassandra/src/com/utad/cassandra/util/Utils.java
+++ cassandra/src/com/utad/cassandra/util/Utils.java
@@ -9,14 +9,22 @@ import com.netflix.astyanax.impl.AstyanaxConfigurationImpl;
 import com.netflix.astyanax.thrift.ThriftFamilyFactory;
 
 public class Utils {
-	
+
 	public static String clusterName = "utad";
-	
-	public static Keyspace keyspace = null;
-	
 
 	public static Keyspace getKeyspace(String name) {
-		
-		return keyspace;
+
+		// Crear la conexión con la BD
+		AstyanaxContext<Keyspace> context = new AstyanaxContext.Builder().forCluster(clusterName).forKeyspace(name)
+				.withAstyanaxConfiguration(new AstyanaxConfigurationImpl().setDiscoveryType(NodeDiscoveryType.RING_DESCRIBE))
+				.withConnectionPoolConfiguration(new ConnectionPoolConfigurationImpl("MyConnectionPool").setPort(9160).setMaxConnsPerHost(1).setSeeds("127.0.0.1:9160"))
+				.withConnectionPoolMonitor(new CountingConnectionPoolMonitor()).buildKeyspace(ThriftFamilyFactory.getInstance());
+
+		context.start();
+		// Crear la conexión con la BD
+
+		// Devolvemos el Keyspace que vamos a utilizar. 
+		// Tenemos que haberlo creado previamente.
+		return context.getClient();
 	}
 }

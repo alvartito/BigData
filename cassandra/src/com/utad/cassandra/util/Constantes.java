4d3a03f9a05fcd81e33e14bbfed0daf1fb647909
Álvaro Sánchez <alvartito@gmail.com>
2015-03-13 09:49:29 -0700

    .

---
+++ cassandra/src/com/utad/cassandra/util/Constantes.java
@@ -0,0 +1,16 @@
+package com.utad.cassandra.util;
+
+public class Constantes {
+
+	/** Nombre del cluster */
+	public static final String clusterName = "utad";
+	/** Nombre del keyspace */
+	public static final String keyspaceName = "utad2";
+	/** Nombre del column family */
+	public static final String columnFamilyName = "users";
+	public static final String columnFamilyName2 = "users2";
+	public static final String columnFamilyName3 = "users3";
+	/** Nombre de la row key */
+	public static final String rowKey = "usersById";
+
+}

3ce1a8b8b6621380cdf409aa8aa55ec256b5cfc9
Álvaro Sánchez <alvartito@gmail.com>
2015-03-13 12:53:10 -0700

    .

--- cassandra/src/com/utad/cassandra/java_driver/UsingJDriver.java
+++ cassandra/src/com/utad/cassandra/java_driver/UsingJDriver.java
@@ -26,35 +26,40 @@ public class UsingJDriver {
 		Session session;
 		session = cluster.connect();
 
-		try {
+//		try {
+			session.execute("drop keyspace utad_cql");
+			
 			// ejecutamos una query directamente, en este caso, para crear el keyspace
 			session.execute("CREATE KEYSPACE utad_cql WITH replication "
-					+ "= {'class':'SimpleStrategy', 'replication_factor':3};");
+					+ "= {'class':'SimpleStrategy', 'replication_factor':1};");
 
 			session.execute("CREATE TABLE utad_cql.users (" + "id int,"
-					+ "email text," + "PRIMARY KEY (id));");
-		} catch (Exception e) {
-			System.out.println("ya existen keyspace y table");
-		}
+					+ "email text, nombre text, apellido text, " + "PRIMARY KEY (id, email, nombre));");
+//		} catch (Exception e) {
+//			System.out.println("ya existen keyspace y table");
+//		}
 
 		// podemos usar prepared statements
 		PreparedStatement ps1 = session
-				.prepare("Insert into utad_cql.users(id, email) values (?, ?)");
+				.prepare("Insert into utad_cql.users(id, email, nombre, apellido) values (?, ?, ?, ?)");
 
 		// y por supuesto, escrituras en batch
 		BatchStatement batch = new BatchStatement();
 
 		for (int i = 0; i < 1000; i++) {
-			batch.add(ps1.bind(i, "user" + i + "@void.com"));
+			batch.add(ps1.bind(i, "user" + i + "@void.com", "nombre_" + i, "apellido_" + i));
 		}
 
 		session.execute(batch);
 
-		ResultSet results = session.execute("SELECT * FROM utad_cql.users ");
+		ResultSet results = session.execute("SELECT * FROM utad_cql.users");
 
 		for (Row row : results) {
 			System.out.println("id : " + row.getInt("id"));
 			System.out.println("email : " + row.getString("email"));
+			System.out.println("nombre : " + row.getString("nombre"));
+			System.out.println("apellido : " + row.getString("apellido"));
+			
 		}
 		System.out.println();
 

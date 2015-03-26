f64942227d461c1dff64fe299903fd33a81b658e
Alvaro <alvartito@gmail.com>
2015-03-26 23:22:28 +0100

    Finalizado el UsingJDriver2

--- cassandra/src/com/utad/cassandra/java_driver/UsingJDriver2.java
+++ cassandra/src/com/utad/cassandra/java_driver/UsingJDriver2.java
@@ -22,7 +22,7 @@ public class UsingJDriver2 {
 
 		List<User> users = new ArrayList<User>();
 
-		for (int i = 0; i < 1000; i++) {
+		for (int i = 0; i < 20; i++) {
 
 			String id = (i + 1) + "";
 			String email = "user" + id + "@void.com";
@@ -46,18 +46,52 @@ public class UsingJDriver2 {
 		}
 
 		// conectar y crear column family
+		Cluster cluster;
+		String node = "127.0.0.1";
 
+		cluster = Cluster.builder().addContactPoint(node).build();
+
+		Session session;
+		session = cluster.connect();
+
+		session.execute("drop keyspace utad_cql");
+
+		session.execute("CREATE KEYSPACE utad_cql WITH replication = {'class':'SimpleStrategy', 'replication_factor':1};");
+
+		session.execute("CREATE TABLE utad_cql.usersByCPAddress (id_usuario int, cp int, nombre text, email text, calle text, primary key(cp, calle, id_usuario));");
+
+		PreparedStatement ps1 = session
+				.prepare("Insert into utad_cql.usersByCPAddress(id_usuario, cp, nombre, email, calle) values (?, ?, ?, ?, ?)");
+
+		BatchStatement batch = new BatchStatement();
+
+		// Realizamos las inserciones en BD
 		for (User user : users) {
-			String id = user.id;
-			String cp = user.cp;
+			int id = Integer.parseInt(user.id);
+			int cp = Integer.parseInt(user.cp);
 			String nombre = user.nombre;
 			String email = user.email;
 			String calle = user.calle;
 
-			// escribir
+			batch.add(ps1.bind(id, cp, nombre, email, calle));
 		}
 
-		// leer el resultado
+		session.execute(batch);
+
+		ResultSet results = session.execute("select id_usuario, nombre, email " +
+//				",cp, calle  " +
+				"from utad_cql.usersByCPAddress where calle='Alcala' and cp=28002");
+
+		// Leemos los datos recuperados.
+		for (Row row : results) {
+			System.out.println("\nid : " + row.getInt("id_usuario"));
+			System.out.println("nombre : " + row.getString("nombre"));
+			System.out.println("email : " + row.getString("email"));
+//			System.out.println("cp : " + row.getInt("cp"));
+//			System.out.println("calle : " + row.getString("calle"));
+		}
+
+		cluster.close();
 	}
 
 }
